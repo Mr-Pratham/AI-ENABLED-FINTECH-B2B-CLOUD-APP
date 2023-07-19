0bf75617b9169e12e0a479f1a48443f4170cac51
pratham <prathamphenomenol@gmail.com>
2023-07-20 00:58:55 +0530

    Changed path of images

--- src/Components/LogoHeader.js
+++ src/Components/LogoHeader.js
@@ -6,10 +6,10 @@ function LogoHeader() {
     <div style={{ padding: 10, backgroundColor: 'white' }}>
       <Grid container>
         <Grid item xs={5}>
-          <img src="./Images/abclogo.svg" alt="abc_logo" style={{ width: '200px', marginLeft: '10px' }} />
+          <img src={process.env.PUBLIC_URL + '/Images/abclogo.svg'} alt="abc_logo" style={{ width: '200px', marginLeft: '10px' }} />
         </Grid>
         <Grid item xs={6}>
-          <img src="./Images/hrclogo.svg" alt="hrc_logo" style={{ width: '150px', marginLeft: '40px' }} />
+          <img src={process.env.PUBLIC_URL + '/Images/hrclogo.svg'} alt="hrc_logo" style={{ width: '150px', marginLeft: '40px' }} />
         </Grid>
       </Grid>
     </div>
